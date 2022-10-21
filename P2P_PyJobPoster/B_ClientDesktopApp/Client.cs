4e1b9da9ce87ab7104d283af93e040089e981f83
Joshua_Macaulay <jmacaulay@hotmail.com.au>
2022-10-21 12:26:25 +0800

    feat: made base client proj for testing server

---
+++ P2P_PyJobPoster/B_ClientDesktopApp/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_ClientDesktopApp
+{
+    public class Client
+    {
+        public Client(string ip_address, int port, string name)
+        {
+            this.ip_address = ip_address;
+            this.port = port;
+            this.name = name;
+        }
+        public string ip_address { get; set; }
+        public int port { get; set; }
+        public string name { get; set; }
+    }
+}
--- P2P_PyJobPoster/B_ClientDesktopApp/MainWindow.xaml.cs
+++ P2P_PyJobPoster/B_ClientDesktopApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using P2P_Library;
+using RestSharp;
 
 namespace B_ClientDesktopApp
 {
@@ -20,9 +26,58 @@ namespace B_ClientDesktopApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        ServiceHost serviceHost;
+        NetTcpBinding tcp;
+        int port;
+        string ip_address;
+        string name;
         public MainWindow()
         {
             InitializeComponent();
+
+            InitializeServiceHost();
+            testWebAPI();
+        }
+
+        private void testWebAPI()
+        {
+            //IPAddress iPAddress = GetIPAddress();
+            Client client = new Client(ip_address, port, name);
+
+            RestClient restClient = new RestClient("https://localhost:44305/");
+            RestRequest restRequest = new RestRequest("api/Clients", Method.Post);
+            restRequest.AddBody(JsonConvert.SerializeObject(client));
+
+            RestResponse restResponse = restClient.Execute(restRequest);
+
+            Console.WriteLine(restResponse.Content);
+
+
+            //Client client = new Client(iPAddress.ToString(), )
+        }
+
+        private IPAddress GetIPAddress()
+        {
+            IPAddress[] hostAddresses = Dns.GetHostAddresses("");
+
+            IPHostEntry hostEntry = System.Net.Dns.GetHostEntry(Dns.GetHostName());
+            
+            return hostEntry.AddressList[0].MapToIPv4();
+        }
+
+        private void InitializeServiceHost()
+        {
+            port = 4000;
+            name = "Josh_PC";
+            tcp = new NetTcpBinding();
+
+            Client_Net client_Net = new Client_Net();
+            serviceHost = new ServiceHost(client_Net);
+
+            ip_address = "net.tcp://localhost/" + port;
+
+            serviceHost.AddServiceEndpoint(typeof(Client_Net_Interface), tcp, ip_address);
+            serviceHost.Open();
         }
     }
 }
