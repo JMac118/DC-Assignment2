6756b38496b3969ab916d8767cf0f7e5d551235a
Joshua_Macaulay <jmacaulay@hotmail.com.au>
2022-10-21 12:24:50 +0800

    feat: implemented remoting library

---
+++ P2P_PyJobPoster/P2P_Library/Client_Net.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P_Library
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple,
+        UseSynchronizationContext = false,
+        InstanceContextMode = InstanceContextMode.Single)]
+    public class Client_Net : Client_Net_Interface
+    {
+        public string GetJobs()
+        {
+            return "Here's a job";
+        }
+    }
+}
---
+++ P2P_PyJobPoster/P2P_Library/Client_Net_Interface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P_Library
+{
+    [ServiceContract]
+    public interface Client_Net_Interface
+    {
+        // Will need to be the Iron Python thing.
+        // Jobs are python jobs, this is a test stub method.
+        [OperationContract]
+        string GetJobs();
+    }
+}
