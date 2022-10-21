0506a49e6b8ffc70e6f9818b33cd1c3e13ba97a0
Joshua_Macaulay <jmacaulay@hotmail.com.au>
2022-10-21 12:24:06 +0800

    feat: implemented db model and controller

---
+++ P2P_PyJobPoster/A_WebServer/Controllers/ClientsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using A_WebServer.Models;
+
+namespace A_WebServer.Controllers
+{
+    public class ClientsController : ApiController
+    {
+        private clientDBEntities db = new clientDBEntities();
+
+        // GET: api/Clients
+        public IQueryable<Client> GetClients()
+        {
+            return db.Clients;
+        }
+
+        /* 
+           * Client needs to input their ip address and port to get a list of all the 
+           * other clients. If they are not in the list, they cannot access the other
+           * clients.
+           */
+        // GET: api/Clients
+   
+        [ResponseType(typeof(List<Client>))]
+        public IHttpActionResult GetClients(Client client)
+        {
+            List<Client> clients = db.Clients.ToList();
+
+            if (clients.Contains(client))
+            {
+                clients.Remove(client);
+                return Ok(clients);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // GET: api/Clients/5
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult GetClient(int id)
+        {
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        // PUT: api/Clients/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutClient(int id, Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != client.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Clients
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult RegisterClient(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Clients.Add(client);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = client.id }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult DeleteClient(int id)
+        {
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            db.Clients.Remove(client);
+            db.SaveChanges();
+
+            return Ok(client);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClientExists(int id)
+        {
+            return db.Clients.Count(e => e.id == id) > 0;
+        }
+    }
+}
--- P2P_PyJobPoster/A_WebServer/Controllers/HomeController.cs
+++ P2P_PyJobPoster/A_WebServer/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Description;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace A_WebServer.Controllers
