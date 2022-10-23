580f9905e2a01a3128bbee71810f9951c4336edb
sharronfoo <wks.foo@gmail.com>
2022-10-23 17:00:29 +0800

    chore: model for view

---
+++ P2P_PyJobPoster/C_SwarmWebViewer/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace C_SwarmWebViewer.Models
+{
+    public class Client
+    {
+        public string? ip_address { get; set; }
+        public int port { get; set; }
+        public string? name { get; set; }
+    }
+}
---
+++ P2P_PyJobPoster/C_SwarmWebViewer/Models/Work_Stat.cs
@@ -0,0 +1,8 @@
+﻿namespace C_SwarmWebViewer.Models
+{
+    public class Work_Stat
+    {
+        public int ClientId { get; set; }
+        public int NumCompleted { get; set; }
+    }
+}
